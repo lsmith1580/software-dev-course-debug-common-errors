7a909f1dbc245fe3578d9eaf2162c4c9e8507843
lsmith1580 <lindseysmith1580@gmail.com>
2025-03-24 16:48:56 -0500

    Add files via upload

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -18,22 +18,22 @@ Think about which debugging methods you found most useful and how you might appl
 
 */
 
-// Programs and Solutions
+// Programs and Solutions (Syntax error)
 
 // Program A
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+console.log("Welcome to the bootcamp");
 
 // What’s Wrong?
+// It is missing a closing parathesis and closing quotes, as well as a semicolon. If those are added it should run. 
 
-
-// Program B
+// Program B (Runtime error)
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+let numbers = [2, 4, 8];
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
@@ -41,22 +41,24 @@ for (let i = 0; i < numbers.length; i++) {
 
 // What’s Wrong?
 
-
+// The "eight" needs to be changed from a string to a number. 
 
 // Program C (Logic Error)
 // Description:
 // This snippet of code is supposed to check if a given number is prime (i.e., divisible only by 1 and itself). However, it incorrectly marks some numbers as prime or not prime.
 
 function isPrime(num) {
-  if (num < 2) return false;
+  if (num < 2) return true;
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
-      return true;  // Supposed to indicate num is NOT prime
+      return false;  // Supposed to indicate num is NOT prime
     }
   }
-  return false; // Supposed to indicate num IS prime
+  return true; // Supposed to indicate num IS prime
 }
 
 console.log(isPrime(7)); // Expected true but gets false
 
 // What’s Wrong?
+
+// There was a logic error, the return false functions should have been return true, and the return true should have been return false. 
